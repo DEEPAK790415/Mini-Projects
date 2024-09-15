6fbbe4e60f8e1d15d68a02337fd729889b3f23c6
DEEPAK B <146861141+DEEPAK790415@users.noreply.github.com>
2024-09-15 11:33:20 +0530

    Update atm project.c

--- atm project.c
+++ atm project.c
@@ -1,26 +1,28 @@
 #include<stdio.h>
-#include<conio.h>
+//#include<conio.h>
 int main()
 {
     int pin,pin1=5555,c=1,ch,amt,balance=5000;
-    printf("Welcome to the bank server\n");
+    printf("----------Welcome to the bank server----------\n");
     start:
     printf("Enter the pin number\n");
     scanf("%d",&pin);
     if(pin==pin1)
     {
-    printf("Please select your option\n");
-    printf("1.Deposit \n2.Withdraw \n3.Check balance \n4.Change pin\n");
+    printf("----------Please select your option----------\n\n");
+    printf("1.Deposit--------->[1]And ENTER \n2.Withdraw-------->[2]And ENTER \n3.Check balance--->[3]And ENTER \n4.Change pin------>[4]And ENTER\n\n");
+    printf("Enter the number-->\n\n");
     scanf("%d",&ch);
     switch(ch)
     {
     case 1:
-           printf("Enter the Deposit amount\n");
+           printf("Enter the Deposit amount----------------------\n");
            scanf("%d",&amt);
            if(amt<=20000)
            {
             balance=balance+amt;
             printf("%d",balance);
+            printf("\n");
            }
            else
             {
@@ -28,16 +30,16 @@ int main()
             }
         break;
     case 2:
-           printf("Enter the Withdraw amount\n");
+           printf("Enter the Withdraw amount--------------------\n");
            scanf("%d",&amt);
            if(amt<=20000)
            {
            balance=balance-amt;
-           printf("After withdraw %d\ncurrent balance=%d\n",amt,balance);
+           printf("After withdraw : %d\ncurrent balance : %d\n",amt,balance);
            }
            else
            {
-            printf("Insufficient balance");
+            printf("----------Insufficient balance----------\n");
            }
 
         break;
@@ -45,7 +47,7 @@ int main()
           printf("Your balance is=%d\n",balance);
           break;
     case 4:
-           printf("Enter the new pin");
+           printf("Enter the new pin--------------------\n");
            scanf("%d",&pin1);
            printf("Your pin is updated successfully...!\nlogin with new pin\n");
            goto start;
@@ -55,7 +57,7 @@ int main()
     }
     else
     {
-    printf("Enter the valid pin number\n");
+    printf("Enter the valid pin number----------------------\n");
     if(c<3)
     {
     c++;
@@ -67,6 +69,5 @@ int main()
  scanf("%d",&ch);
  if(ch==1)
  goto start;
- printf("Thanks for visiting our ATM");
+ printf("!...Thanks for visiting our ATM...!");
 }
-
